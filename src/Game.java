bbefa2abc8a4bb444ab54b7a6008845916989d50
nursh <nursh@github.com>
2016-05-18 23:59:29 -0400

    'added punishments for game loser'

--- src/Game.java
+++ src/Game.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 public class Game {
 
-    public static final char X = 'x';
-    public static final char O = 'o';
+    public static final char CROSS = 'X';
+    public static final char NOUGHT = 'O';
     public static final int ROWS = 3;
     public static final int COLUMNS = 3;
 
+    //add winning bets to be done after loosing in an array.
+    public List<String> punishments;
     private char[][] gameBoard;
     private Player[] players;
     private Set<Integer> playedPositions;
@@ -15,6 +17,7 @@ public class Game {
         gameBoard = new char[ROWS][COLUMNS];
         playedPositions = new HashSet<>();
         this.players = new Player[2];
+        punishments = new ArrayList<>();
         setPlayers(players);
         initialize();
     }
@@ -28,6 +31,21 @@ public class Game {
         }
     }
 
+    private void useDefaultPunishments() {
+        punishments.add("Dish out a dirty little secret of yours.");
+        punishments.add("Bend over backwards and kiss the wall.");
+        punishments.add("Inhale my Smelly sock fumes.");
+        punishments.add("Undress and wiggle, wiggle, wiggle while saying shouting \"Do I look sexy now\". ");
+        punishments.add("Eat the hot sauce.");
+    }
+
+    public void printPunishments() {
+        System.out.println("The sucker does the deed.");
+        printSeparator();
+        for(String p : punishments) {
+            System.out.println(p);
+        }
+    }
     private void setPlayers(Player[] play) {
         int i = 0;
         for(Player p: play) {
@@ -56,18 +74,19 @@ public class Game {
     }
 
     private Player findFirstPlayer() {
-        if (players[0].getPiece() == X) return players[0];
+        if (players[0].getPiece() == CROSS) return players[0];
         return players[1];
     }
 
     private static void printSeparator() {
+
         System.out.println("----------------------------------------\n");
     }
 
     public void play() {
         Player presentPlayer = findFirstPlayer();
         printSeparator();
-        System.out.println("\n'X' starts the game.");
+        System.out.println("\n'CROSS' starts the game.");
         printSeparator();
         Scanner sc = new Scanner(System.in);
         this.print();
@@ -75,6 +94,7 @@ public class Game {
                 System.out.println("Enter a position on the board");
                 System.out.print(presentPlayer.getName() + ": ");
                 int position = sc.nextInt();
+                printSeparator();
                 if ((playedPositions.add(position))) {
                     this.setPieceAtPosition(presentPlayer, position);
                     this.print();
@@ -84,7 +104,7 @@ public class Game {
                 }
         }
 
-        if (this.isBoardFull()) {
+        if (this.isBoardFull() && this.gameOver() == false) {
             System.out.println("You both suck!!!\n");
             System.out.println("This game was a draw");
         }
@@ -93,7 +113,7 @@ public class Game {
     public boolean isBoardFull() {
         for (char[] c : gameBoard) {
             for (char p : c) {
-                if (p != X && p != O) return false;
+                if (p != CROSS && p != NOUGHT) return false;
             }
         }
         return true;
@@ -109,6 +129,8 @@ public class Game {
 
     }
 
+
+    //refactor to get winner and the piece that won.
     public boolean gameOver() {
         return  this.getPieceAtPosition(0) == this.getPieceAtPosition(1) && this.getPieceAtPosition(0) == this.getPieceAtPosition(2) ||
                 this.getPieceAtPosition(0) == this.getPieceAtPosition(3) && this.getPieceAtPosition(0) == this.getPieceAtPosition(6) ||
@@ -123,28 +145,31 @@ public class Game {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to intense 'X' and 'O'.");
+        System.out.println("Welcome to extreme 'Noughts and Crosses'.");
         printSeparator();
 
         System.out.print("Enter player 1 name: ");
-        String p1Name= sc.next();
+        String p1Name= sc.nextLine();
         System.out.print(p1Name +  ", choose (x or o): ");
-        char p1Piece = sc.next().toLowerCase().charAt(0);
+        char p1Piece = sc.nextLine().toUpperCase().charAt(0);
         Player p1 = new Player(p1Name, p1Piece);
         printSeparator();
 
         System.out.print("Enter player 2 name: ");
-        String p2Name= sc.next();
+        String p2Name= sc.nextLine();
         char p2Piece;
-        if (p1Piece == 'x') {
-            p2Piece = 'o';
+        if (p1Piece == CROSS) {
+            p2Piece = NOUGHT;
         } else {
-            p2Piece = 'x';
+            p2Piece = CROSS;
         }
         System.out.println(p2Name + ", you are using: " + p2Piece);
         Player p2 = new Player(p2Name, p2Piece);
         Player[] players = {p1, p2};
         Game game = new Game(players);
+        printSeparator();
+        game.useDefaultPunishments();
+        game.printPunishments();
         game.play();
 
     }
